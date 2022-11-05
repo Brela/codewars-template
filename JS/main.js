1005307298fcf2ab027a56cb6e5e4d36bfe4463f
Breland <claybreland1@gmail.com>
2022-11-05 13:53:34 -0500

    looking good

--- JS/main.js
+++ JS/main.js
@@ -1,65 +1,31 @@
-//prevent default page reload on click
-/* document.querySelector('button').addEventListener('click', function handleClick(event) {
-    event.preventDefault();
-});
- */
 
 //===========================================================================================================
 
+const arrOfListItems = document.querySelectorAll('.listItem')
 
-const itemList = document.querySelectorAll('.dropdown')
 
-function clearStick() {
-    const stickItems = document.querySelectorAll('.dropdown-content')
+//                             -----------------------------------------------
+//convert listItemID to displayContentClass (these should match in HTML), call clearStick, 
+//then toggle its stick (display: block)
+
+arrOfListItems.forEach((el, i) => el.addEventListener('click', () => {
+    var listItemID = arrOfListItems[i].id
+    var displayContentClass = '.' + listItemID
+    clearPrevListItemSelectedColor()
+    clearPrevDisplayedContent()
+    document.getElementById(listItemID).classList.toggle('listItemSelectedColor')
+    document.querySelector(displayContentClass).classList.toggle('stick')
+
+}))
+function clearPrevListItemSelectedColor() {
+    const listItemsSelected = document.querySelectorAll('.listItem')
+    listItemsSelected.forEach(el => {
+        el.classList.remove('listItemSelectedColor')
+    })
+}
+function clearPrevDisplayedContent() {
+    const stickItems = document.querySelectorAll('.displayContent')
     stickItems.forEach(el => {
         el.classList.remove('stick')
     })
 }
-//                             -----------------------------------------------
-//convert aDate (ID) to xDate (CLASS), call clearStick, 
-//then toggle its stick (display: block)
-
-itemList.forEach((el, index) => el.addEventListener('click', () => {
-    var aDate = itemList[index].id
-    var xDate = '.' + aDate.replace(aDate[0], 'x')
-    clearStick()
-    document.querySelector(xDate).classList.toggle('stick')
-
-}))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// notes
-
-//do function on 'enter' key hit
-
-/* When the keypress event occurs, the event object will contain the data of which
-key is pressed. The code of the pressed key will be present in keyCode or the which
-property of the event object. */
-/* document.querySelector('#addNumsHere').addEventListener('keypress', (event) => {
-    let keyCode = event.keyCode ? event.keyCode : event.which;
-    //So, we check if the pressed keycode is equal to the Enter key code (13)
-    if (keyCode === 13) {
-        // call click function on Enter
-        return addNums();
-    }
-}); */
-
-
--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# d-d_template_prototype
-
-created using my codeWars project as a template
 
